4458358100c9be28558ab2ad60a543604cb5922f
AsadAmin <asadamin_uk@hotmailcom>
2022-11-19 20:11:39 +0000

    Initial barebone api

---
+++ AirportCarParking/AirportCarParking/Controllers/ParkingReservationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AirportCarParking.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ParkingReservationController : ControllerBase
+    {
+        private readonly ILogger<ParkingReservationController> _logger;
+
+        public ParkingReservationController(ILogger<ParkingReservationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello World";
+        }
+    }
+}

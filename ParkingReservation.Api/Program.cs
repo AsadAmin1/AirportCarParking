dc1cf99ca8d3543a5740cb99d983d619fee90800
AsadAmin <asadamin_uk@hotmailcom>
2022-11-22 22:46:44 +0000

    Fixed namespaces after rename of folder

--- ParkingReservation.Api/Program.cs
+++ ParkingReservation.Api/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace AirportCarParking
+namespace ParkingReservation.Api
 {
     public class Program
     {
--- ParkingReservation.Api/Startup.cs
+++ ParkingReservation.Api/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace AirportCarParking
+namespace ParkingReservation.Api
 {
     public class Startup
     {
